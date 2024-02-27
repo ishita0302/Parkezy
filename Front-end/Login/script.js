38903ab20648aa6167e9c3401fe6dbab542b62c9
K-R-I-T-I-K-A <116299052+KritikaSanghi@users.noreply.github.com>
2024-02-27 21:46:05 +0530

    Update script.js

--- Front-end/Login/script.js
+++ Front-end/Login/script.js
@@ -1,1 +1,18 @@
 
+        function togglePasswordVisibility() {
+            var passwordInput = document.getElementById("passwordInput");
+            passwordInput.type = passwordInput.type === "password" ? "text" : "password";
+        }
+
+        function validateForm() {
+            var passwordInput = document.getElementById("passwordInput");
+            var password = passwordInput.value;
+            var passwordRegex = /^(?=.*[!@#$%^&*(),.?":{}|<>])(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/;
+
+            if (!passwordRegex.test(password)) {
+                alert("Password must have at least one special character, one uppercase letter, one lowercase letter, and one number");
+                return false;
+            }
+
+            return true;
+        }
